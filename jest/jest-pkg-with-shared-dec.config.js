a0b4046ae5658588f8e865279f8f521032ad0176
Brad Hezh <bradhezh@gmail.com>
2026-01-23 01:36:57 +1300

    renamed

---
+++ jest/jest-pkg-with-shared-dec.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.[jt]sx?$": [
+      "@swc/jest",
+      {
+        module: { type: "commonjs" },
+        jsc: {
+          parser: { syntax: "typescript", decorators: true },
+          transform: { legacyDecorator: true, decoratorMetadata: true },
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@shared/(.*)$": "<rootDir>/../shared/src/$1",
+  },
+  testTimeout: 5000,
+};
---
+++ jest/jest-pkg-with-shared.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.[jt]sx?$": ["@swc/jest", { module: { type: "commonjs" } }],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@shared/(.*)$": "<rootDir>/../shared/src/$1",
+  },
+  testTimeout: 5000,
+};
